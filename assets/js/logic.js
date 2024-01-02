71cae1d998c1f405e13e0831ea4bb4a080d5f327
fuadeyuboglu <fuadeyuboglu@gmail.com>
2024-01-02 18:54:30 +0000

    Created logic to fetch question and its answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,30 @@
+const questions = [
+    {'question': '5 X 5',
+        'options': [
+        '15',
+        '35',
+        '20',
+        '25'],
+        'answer': '25'
+    },
+    
+    {'question': '3 X 5',
+        'options': [
+        '15',
+        '35',
+        '20',
+        '25'],
+        'answer': '15'
+    }, 
+]
+
+
 const startButton = document.getElementById('start');
 const startScreen = document.getElementById('start-screen');
 const questionScreen = document.getElementById('questions');
 const time = document.getElementById('time');
+const questionTitle = document.getElementById('question-title');
+const questionChoices = document.getElementById('choices');
 
 
 
@@ -14,4 +37,18 @@ startButton.addEventListener('click', () => {
         seconds = seconds - 1;
         time.innerHTML = seconds;
     }, 1000);
+
+
+    // Fetching question with answers
+    questionTitle.innerText = questions[0].question + ' = ?';
+    
+    choiceList = '';
+
+    for (let i = 0; i <= 3; i++) {
+        choiceList += `<button>${questions[0].options[i]}</button>`;
+    }
+
+    questionChoices.innerHTML = `${choiceList}`;
+
+
 })
